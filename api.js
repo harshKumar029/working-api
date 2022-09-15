855620aa7859ac2225f142af3a326d6559fd071e
Harsh Kumar <113521435+harshKumar029@users.noreply.github.com>
2022-09-15 18:20:13 +0530

    Api

---
+++ api.js
@@ -0,0 +1,11 @@
+// var element;
+
+
+// document.getElementById("apibody").onclick = change;
+// function change(){
+//     //  document.getElementById("apibody").style.background = "red";
+//      location.href = "http://127.0.0.1:5500/gym/gym.html";
+// }
+// function myFunction() {
+//     location.href = "https://www.w3schools.com";
+//   }
